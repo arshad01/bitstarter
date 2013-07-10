84608c323360ecb5e6550ffe3047d752cf44246d
arshad01 <khan.m.arshad@gmail.com>
2013-07-10 14:24:45 +0000

    Read from index.htm

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var filename = 'index.html';
+var buffer = fs.readFileSync(filename);
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
